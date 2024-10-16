d79939f9823900ebb8a53ca265f0a34b445ef65e
AimsCloudDrive <i79401045@126.com>
2024-10-16 15:27:34 +0800

    update common

--- modules/common/src/assert/index.ts
+++ modules/common/src/assert/index.ts
@@ -2,7 +2,7 @@ export function assert(
   condition: any,
   message: string = ""
 ): asserts condition {
-  if (!condition) throw Error(message);
+  if (condition === undefined) throw Error(message);
 }
 
 export function nil<T>(data: T | undefined, defaultData: T): T {
