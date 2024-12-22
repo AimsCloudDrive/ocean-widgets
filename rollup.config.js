7e9f5a4e46adb4740573fb23a20ffd9cf3d0ff23
AimsCloudDrive <i79401045@126.com>
2024-12-22 15:28:00 +0800

    update

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,10 @@ import commonjs from "@rollup/plugin-commonjs";
 import babel from "@rollup/plugin-babel";
 
 export default {
+  external: (id) => {
+    // 使用正则表达式来匹配所有以 @ocean/ 开头的模块
+    return /^@ocean\//.test(id);
+  },
   input: "src/index.ts", // 你的主要输入文件
   output: {
     sourcemap: true,
@@ -19,16 +23,20 @@ export default {
       targets: ["defaults"],
       exclude: "node_modules/**",
       extensions: [".ts", ".js", ".tsx", ".jsx"],
-      presets: ["@babel/preset-env", "@babel/preset-typescript"],
+      presets: [
+        "@babel/preset-env",
+        ["@babel/preset-typescript", { allowDeclareFields: true }],
+      ],
       plugins: [["@babel/plugin-proposal-decorators", { version: "legacy" }]],
       babelrc: false,
       sourceMaps: true,
+      babelHelpers: "bundled",
     }),
     typescript({
       tsconfig: "./tsconfig.json",
       declaration: true,
       declarationDir: "dist/types", // 声明文件的路径
-      rootDir: "src",
+      // rootDir: "src",
       outDir: "dist",
       sourceMap: true,
     }),
