b9d8983496ad2f4614bffc554f2f444aff71c40d
AimsCloudDrive <i79401045@126.com>
2025-08-11 17:55:34 +0800

    .

--- modules/ui/src/layout/ScrollLayout/ScrollBar/index.tsx
+++ modules/ui/src/layout/ScrollLayout/ScrollBar/index.tsx
@@ -5,9 +5,10 @@ import {
   component,
   ComponentEvents,
 } from "@msom/component";
-import { observer } from "@msom/reaction";
+import { createReaction, observer } from "@msom/reaction";
 import { Vec2 } from "../../AnchorLayout/types";
 import "ScrollBar.less";
+import { SingleRef, createSingleRef } from "@msom/dom";
 
 type Vertical = "Vertical";
 type Horizontal = "Horizontal";
@@ -22,12 +23,12 @@ const Absolute: Absolute = "Absolute";
 
 type ScrollBarProps = ComponentProps & {
   aspect?: Aspect;
-  contentSize: number | Vec2;
-  currentPosition: number | Vec2;
+  contentSize: number;
+  currentPosition: number;
   barPosition?: BarPosition;
 };
 type ScrollBarEvents = ComponentEvents & {
-  positionChange: Vec2;
+  positionChange: number;
 };
 
 @component("scroll-bar")
@@ -37,17 +38,23 @@ export class ScrollBar extends Component<ScrollBarProps, ScrollBarEvents> {
   declare aspect: Aspect;
   @option()
   @observer()
-  declare contentSize: Vec2;
+  declare contentSize: number;
   @option()
   @observer()
-  currentPosition: Vec2;
+  currentPosition: number;
   @option()
   @observer()
   declare barPosition: BarPosition;
+
+  private declare track: SingleRef<HTMLDivElement>;
+  private declare thumb: SingleRef<HTMLDivElement>;
+
   init() {
     super.init();
     this.aspect = "Vertical";
     this.barPosition = "Normal";
+    this.track = createSingleRef();
+    this.thumb = createSingleRef();
   }
 
   render() {
@@ -59,10 +66,43 @@ export class ScrollBar extends Component<ScrollBarProps, ScrollBarEvents> {
             this.aspect.toLocaleLowerCase(),
             this.barPosition.toLocaleLowerCase(),
           ]}
+          $ref={this.track}
         >
-          <div class={["scroll-bar-thumb"]}></div>
+          <div
+            $ref={this.thumb}
+            class={["scroll-bar-thumb"]}
+            style={{ [this.keys.length]: this.thumbSize }}
+          ></div>
         </div>
       </div>
     );
+  }
+  private get keys() {
+    if (this.aspect === "Vertical") {
+      return { position: "top", length: "height" };
+    } else {
+      return {
+        position: "left",
+        length: "width",
+      };
+    }
+  }
+  @observer({ initValue: 0 })
+  private declare containerSize: number;
+  @observer({ initValue: 0 })
+  private declare thumbSize: number;
+
+  update() {
+    const { track, thumb } = this;
+    if (!track.data || !thumb.data) {
+      return;
+    }
+    const containerRect = this.track.data.getBoundingClientRect();
+    this.containerSize = containerRect[this.keys.length];
+    this.thumbSize =
+      (this.containerSize / this.contentSize) * this.containerSize;
+  }
+  mounted() {
+    this.update();
   }
 }
